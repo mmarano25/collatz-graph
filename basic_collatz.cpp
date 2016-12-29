4aeca8dfecda840d82f887f0b8c4aedd1d984241
mmarano25 <mmarano@uci.edu>
2016-12-29 13:32:53 -0800

    refactoring, messing around

--- basic_collatz.cpp
+++ basic_collatz.cpp
@@ -1,23 +1,49 @@
 #include <iostream>
+#include <iterator>
+#include <vector>
+#include <algorithm>
 
-void collatz( int n )
+
+// utility functions 
+	template< typename T >
+void print_vector( std::vector<T> v )
 {
-	std::cout << n << ' ';
+	std::copy( std::begin( v ), std::end( v ),
+			std::ostream_iterator<T>( std::cout, " " ) );
+}
+
+int collatz_step( int n )
+{ //executes one step of the collatz conject
+	if( (n % 2 ) == 0 )
+		return ( n / 2 );
+	else
+		return ( ( n * 3 ) + 1 );
+}
+
+
+
+// main functions 
+std::vector<int> store_collatz( int n )
+{
+	std::vector<int> result;
+	result.push_back( n );
 	while( n != 1 )
 	{
-		if( (n % 2) == 0 )
-			n = n / 2;
-		else
-			n = (n * 3) + 1;
-		std::cout << n << ' ';
+		n = collatz_step( n );
+		result.push_back( n );
 	}
+	return result;
 }
+
 
 int main( )
 {
 	int n;
 	std::cout << "Enter a number: ";
 	std::cin >> n;
-	collatz( n );
+	std::vector<int> stored_collatz = store_collatz( n );
+
+	print_vector<int>( stored_collatz );
+	std::cout << std::endl;
 	return 0;
 }
