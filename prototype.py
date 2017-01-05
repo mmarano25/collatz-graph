ba60de3fc0153a2ecdaea0abe519afd0fca75f56
mmarano25 <mmarano@uci.edu>
2017-01-05 10:48:02 -0800

    skeleton of all python sketched out

---
+++ prototype.py
@@ -0,0 +1,15 @@
+import plotly as plt
+import plotly.graph_objs as go
+
+
+def collatz_steps(num: int) -> int:
+    """runs the collatz conjecture on num, returns number of steps"""
+    return
+
+plotly_data = [
+    go.Histogram(
+        """data here"""
+    )
+]
+
+plt.offline.plot(plotly_data)
