0f7c7156aa20578c32e3f91e6a54e9b5764a3ce8
mmarano25 <mmarano@uci.edu>
2017-01-05 14:35:54 -0800

    began work on collatz_gui

--- collatz_gui.py
+++ collatz_gui.py
@@ -1,2 +1,27 @@
 import tkinter as tk
 
+title_text = """Enter a number.
+The histogram will display the stopping points for the Collatz theorem up until that number."""
+
+
+class CollatzGui(tk.Frame):
+    def __init__(self, root=None):
+        tk.Frame.__init__(self, root)
+        self.create_widgets()
+
+    def create_widgets(self):
+        title_label = tk.Label(self, text=title_text, anchor="c")
+        entry_stringvar = tk.StringVar()
+        entry_box = tk.Entry(self, textvariable=entry_stringvar, justify=tk.CENTER)
+        self.enter_button = tk.Button(self, text="Go!")
+
+        title_label.pack(side="top", fill="both", expand=True)
+        entry_box.pack(side="top", fill="both", expand=True)
+        self.enter_button.pack(side="top", fill="both", expand=True)
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    view = CollatzGui(root)
+    view.pack(side="top", fill="both", expand=True)
+    root.mainloop()
