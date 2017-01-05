d71ae2ab60620d5f81a4b3004f5e38d9b96c659e
mmarano25 <mmarano@uci.edu>
2017-01-05 10:53:56 -0800

    finished collatz_steps function, added tests and global testing variable

--- prototype.py
+++ prototype.py
@@ -1,15 +1,35 @@
 import plotly as plt
 import plotly.graph_objs as go
 
+TESTS_ON = True
 
 def collatz_steps(num: int) -> int:
     """runs the collatz conjecture on num, returns number of steps"""
-    return
+    if num == 0:
+        return 0
+    n = 0
+    while num != 1:
+        n += 1
+        if (num % 2) == 0:
+            num /= 2
+        else:
+            num = (num * 3) + 1
+    return n
 
+
+# tests:
+if TESTS_ON:
+    assert collatz_steps(0) == 0
+    assert collatz_steps(10) == 6
+    assert collatz_steps(1000) == 111
+    assert collatz_steps(123214) == 211
+
+"""
 plotly_data = [
     go.Histogram(
-        """data here"""
+        "data here"
     )
 ]
 
 plt.offline.plot(plotly_data)
+"""
